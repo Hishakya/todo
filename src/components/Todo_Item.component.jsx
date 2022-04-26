94e83433890990c14f3dfa01af7268cdb7d296c6
Hi ! Shakya <hishakya@His-MacBook-Air.local>
2022-04-26 18:15:02 +0530

    todo3

--- src/components/Todo_Item.component.jsx
+++ src/components/Todo_Item.component.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const TodoItem = ({ todo }) => {
   return (
-    <React.Fragment>
+    <>
       <div >
         {todo}
       </div>
-    </React.Fragment>
+    </>
   );
 };
 
